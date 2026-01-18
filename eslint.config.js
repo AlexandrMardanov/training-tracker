34e9fd0947fb4281f857c552f6d9bc274bcf9f14
Oleksandr Mardanov <oleksandr.mardanov@patrianna.com>
2026-01-18 03:45:52 +0200

    Refactor project configuration and remove unused files
    - Cleaned up `app.json` by consolidating plugin entries.
    - Deleted unnecessary files: `babel.config.js`, `global.css`, `index.ts`, `metro.config.js`, `nativewind-env.d.ts`, and `tailwind.config.js`.
    - Updated `package.json` to remove dependencies related to removed files and added ESLint and Prettier configurations.
    - Refined ESLint configuration to include Prettier support.
    - Enhanced styling in `index.tsx` with React Native's `StyleSheet` for better performance and maintainability.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
-// https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require("eslint/config");
-const expoConfig = require("eslint-config-expo/flat");
-const reactCompiler = require("eslint-plugin-react-compiler");
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const reactCompiler = require('eslint-plugin-react-compiler');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   reactCompiler.configs.recommended,
   {
-    ignores: ["dist/*"],
+    ignores: ['dist/*'],
   },
 ]);
