e1345f3062a7c50974a1f47f51d447f85f688bb6
Oleksandr Mardanov <oleksandr.mardanov@patrianna.com>
2026-02-01 14:36:12 +0200

    feat: dynamically set dashboard title based on user metadata

--- app/(tabs)/(dashboard)/index.tsx
+++ app/(tabs)/(dashboard)/index.tsx
@@ -1,10 +1,13 @@
 import { DashboardScreen } from '@/components/dashboard/DashboardScreen';
 import { ProfileButton } from '@/components/dashboard/components/ProfileButton';
+import { useAuth } from '@/contexts/AuthContext';
 import { useScreenOptions } from '@/hooks/useScreenOptions';
 
 export default function Dashboard() {
+  const { user } = useAuth();
+
   useScreenOptions({
-    title: 'Дашборд',
+    title: user?.user_metadata.name || 'Дашборд',
     headerRight: () => <ProfileButton />,
   });
 
